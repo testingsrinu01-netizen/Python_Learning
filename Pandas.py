c2fdc60d82e50ea045586fa99b6ab4ba52b167d1
Srinivas G <testing.srinu01@gmail.com>
2025-08-18 22:03:05 +0530

    Python Learning update

--- Pandas.py
+++ Pandas.py
@@ -6,14 +6,15 @@ studentData = {
 "Name":["Alice","Bob","Mary"],
 "Math":[60,80,90],
 "Physics":[10,77,56],
-"Chemistry":[90,90,90],
+"Chemistry":[96,90,90],
 }
 
 df = pd.DataFrame(studentData);
 print(df)
 
-print(df.head(2)); #Print 2 rows apart from top of table.
-print(df.tail(1)); #Print 1 rows apart from bottom of table.
+print(df.head(2)); #Print 2 rows from top of table.
+print(df.tail(1)); #Print 1 rows from bottom of table.
 print(df.shape); # (rows, columns)
 print(df.columns);  # list of column names
 print(df.keys());
+print(df.dropna(axis=1));
