5549a33b1ac044c340691653addee8a11016298b
Srinivas G <testing.srinu01@gmail.com>
2025-08-24 20:07:39 +0530

    Python Learning pandas file updated

--- Pandas.py
+++ Pandas.py
@@ -4,9 +4,9 @@ import pandas as pd;
 
 studentData = {
 "Name":["Alice","Bob","Mary"],
-"Math":[60,80,90],
+"Math":[60,80,None],
 "Physics":[10,77,56],
-"Chemistry":[96,90,90],
+"Chemistry":[96,86,90],
 }
 
 df = pd.DataFrame(studentData);
@@ -17,5 +17,6 @@ print(df.tail(1)); #Print 1 rows from bottom of table.
 print(df.shape); # (rows, columns)
 print(df.columns);  # list of column names
 print('Keys from table ::- ', df.keys());
+#df = df.dropna(axis=1);   #This assignment will persist the df.
 print(df.dropna(axis=1));
 print(df);
